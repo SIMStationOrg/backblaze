e446bd34c0291b835d3b8ee853176f17a0019702
Moritz Reis <me@moritzreis.dev>
2025-02-19 12:30:51 +0100

    fix: typo

--- src/Client/Models/Requests/GetDownloadAuthorizationRequest.cs
+++ src/Client/Models/Requests/GetDownloadAuthorizationRequest.cs
@@ -14,7 +14,7 @@ namespace Bytewizer.Backblaze.Models
         /// <summary>
         /// Initializes a new instance of the <see cref="GetDownloadAuthorizationRequest"/> class.
         /// </summary>
-        /// <param name="bucketId">The buckete id the download authorization token will allow access.</param>
+        /// <param name="bucketId">The bucket id the download authorization token will allow access.</param>
         /// <param name="fileNamePrefix">The file name prefix of files the download authorization token will allow access.</param>
         /// <param name="validDurationInSeconds">The number of seconds before the authorization token will expire.</param>
         public GetDownloadAuthorizationRequest(string bucketId, string fileNamePrefix, long validDurationInSeconds = 3600)
@@ -46,26 +46,26 @@ namespace Bytewizer.Backblaze.Models
         /// </summary>
         /// <remarks>
         /// For example, if you have a private bucket named "photos" and generate a download authorization token for the
-        /// fileNamePrefix "pets/" you will be able to use the download authorization token 
-        /// to access: https://f345.backblazeb2.com/file/photos/pets/kitten.jpg 
-        /// but not: https://f345.backblazeb2.com/file/photos/vacation.jpg. 
+        /// fileNamePrefix "pets/" you will be able to use the download authorization token
+        /// to access: https://f345.backblazeb2.com/file/photos/pets/kitten.jpg
+        /// but not: https://f345.backblazeb2.com/file/photos/vacation.jpg.
         /// </remarks>
         [JsonProperty(Required = Required.Always)]
         public string FileNamePrefix { get; private set; }
 
         /// <summary>
         /// The number of seconds before the authorization token will expire. The minimum value is 1 second.
-        /// The maximum value is 604800 which is one week in seconds. 
+        /// The maximum value is 604800 which is one week in seconds.
         /// </summary>
         [JsonProperty(Required = Required.Always)]
         public long ValidDurationInSeconds { get; private set; } = 3600;
 
         /// <summary>
-        /// If this is present B2 will use it as the value of the Content-Disposition header when the file is downloaded 
+        /// If this is present B2 will use it as the value of the Content-Disposition header when the file is downloaded
         /// unless it's overridden by a value given in the download request. Parameter continuations (contain an '*') are not supported.
         /// </summary>
         /// <remarks>
-        /// Note that this file info will not be included in downloads as a x-bz-info-b2-content-disposition header. 
+        /// Note that this file info will not be included in downloads as a x-bz-info-b2-content-disposition header.
         /// Instead, it (or the value specified in a request) will be in the Content-Disposition
         /// </remarks>
         [JsonIgnore]
